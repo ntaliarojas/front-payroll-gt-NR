0e76a5992762bdd5f63ab8be1b672dde851372e9
ntaliarojas <llerlynatisrojaspuertas@gmail.com>
2024-03-27 22:17:22 -0500

    Resolviendo conflictos2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
- 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -12,5 +12,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Configurar el host para escuchar en todas las interfaces
     port: 3000, // Configurar el puerto que deseas utilizar
+  },
+  build: {
+    // Configuración de optimización para la producción
+    // Habilitar la eliminación de código muerto
+    minify: 'terser',
+    // Opcional: habilitar la eliminación de código muerto específicamente para los módulos de producción
+    target: 'esnext',
   },
 });
